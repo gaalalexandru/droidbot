467492b1d9e5b1e9ab91e2628a9b7b0021965a14
Alexandru Gaal <gsx_16@yahoo.com>
2016-01-07 16:37:04 +0200

    created 50 ms cyclic function container

--- modules/cyclic_activity_handler/cyclic_activity_handler.c
+++ modules/cyclic_activity_handler/cyclic_activity_handler.c
@@ -20,7 +20,7 @@
 extern char print_flag;
 
 /*-------------------Function Definitions-------------*/
-void CYCL_1_second(void)	//Fucntion container, with everithing that executes at 1 second interval
+void CYCL_1_second(void)	//Fucntion container, with everything that executes at 1 second interval
 {
 	static unsigned char counter = 0;
 	if (counter <=30)
@@ -57,7 +57,28 @@ void CYCL_1_second(void)	//Fucntion container, with everithing that executes at 
 	}
 }
 
-void CYCL_50_milisecond(void)
+void CYCL_50_milisecond(void)	//Fucntion container, with everything that executes at 50 mili second interval
 {
-	
+	static unsigned char counter = 0;
+	if((counter%2)==0)
+	{
+		//Code that runs only every 100 ms
+	}
+	if((counter%5)==0)
+	{
+		//Code that runs only every 250 ms
+	}
+	if((counter%10)==0)
+	{
+		//Code that runs only every 500 ms
+	}
+	if(counter == 254)	//Max value reached (Not 255, to keep 2s, 5s and 10s interval
+	{
+		counter = 5;			//Reset counter to 5, to keep 2s, 5s and 10s interval
+	}
+	else
+	{
+		counter++;				//Just increment counter
+	}
+
 }
